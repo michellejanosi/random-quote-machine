dca0aed61329ff4891e58bfd89f6b0b7412e82d1
michellejanosi <michelle.janosi@gmail.com>
2016-12-18 06:42:57 -0500

    Fixed styling on author

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ var quotes = [
   },
   {
     quote: "Choose a job you love and you will never have to work a day in your life",
-    author: "– Confucius"
+    author: "—Confucius"
   },
   {
     quote: "Believe you can and you're halfway there.",
@@ -57,15 +57,15 @@ var quotes = [
   },
   {
     quote: "What would you do if you weren't afraid?",
-    author: "-Sheryl Sandberg"
+    author: "—Sheryl Sandberg"
   },
   {
     quote: "Be who you are and say what you feel, because those who mind don’t matter and those who matter don’t mind.",
-    author: "-Dr. Seuss"
+    author: "—Dr. Seuss"
   },
   {
     quote: "The best way to predict the future is to create it.",
-    author: "-Peter Drucker"
+    author: "—Peter Drucker"
   },
   {
     quote: "Opportunities are usually disguised as hard work, so most people don’t recognize them.",
@@ -77,43 +77,43 @@ var quotes = [
   },
   {
     quote: "The people who are crazy enough to think they can change the world are the ones who do.",
-    author: "-Rob Siltanen"
+    author: "—Rob Siltanen"
   },
   {
     quote: "Fake it until you make it! Act as if you had all the confidence you require until it becomes your reality.",
-    author: "-Brian Tracy"
+    author: "—Brian Tracy"
   },
   {
     quote: "Tough times never last, but tough people do.",
-    author: "-Dr. Robert Schuller"
+    author: "—Dr. Robert Schuller"
   },
   {
     quote: "Everything you’ve ever wanted is on the other side of fear.",
-    author: "-George Addair"
+    author: "—George Addair"
   },
   {
     quote: "It is never too late to be what you might have been.",
-    author: "-George Eliot"
+    author: "—George Eliot"
   },
   {
     quote: "Your imagination is your preview of life’s coming attractions.",
-    author: "-Albert Einstein"
+    author: "—Albert Einstein"
   },
   {
     quote: "Change your thoughts and you change your world.",
-    author: "-Norman Vincent Peale"
+    author: "—Norman Vincent Peale"
   },
   {
     quote: "Don’t wish it were easier, wish you were better.",
-    author: "-Jim Rohn"
+    author: "—Jim Rohn"
   },
   {
     quote: "What the mind of man can conceive and believe, it can achieve.",
-    author: "-Napoleon Hill"
+    author: "—Napoleon Hill"
   },
   {
     quote: "It always seems impossible until its done.",
-    author: "-Nelson Mandela"
+    author: "—Nelson Mandela"
   },
   {
     quote: "The only person you are destined to become is the person you decide to be.",
@@ -121,67 +121,67 @@ var quotes = [
   },
   {
     quote: "Take the first step in faith. You don't have to see the whole staircase, just take the first step.",
-    author: "-Martin Luther King, Jr."
+    author: "—Martin Luther King, Jr."
   },
   {
     quote: "A year from now you may wish you had started today.",
-    author: "-Karen Lamb"
+    author: "—Karen Lamb"
   },
   {
     quote: "Great opportunities are not seen with your eyes. They are seen with your mind.",
-    author: "-Robert T. Kiyosaki"
+    author: "—Robert T. Kiyosaki"
   },
   {
     quote: "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
-    author: "-Thomas Edison"
+    author: "—Thomas Edison"
   },
   {
     quote: "A winner is just a loser who tried one more time.",
-    author: "-George M. Moore Jr."
+    author: "—George M. Moore Jr."
   },
   {
     quote: "It's time to start living the life you've imagined.",
-    author: "-Henry James"
+    author: "—Henry James"
   },
   {
     quote: "You must find a place in yourself where nothing is impossible.",
-    author: "-Deepak Chopra"
+    author: "—Deepak Chopra"
   },
   {
     quote: "It's never too late- never too late to start over, never too late to be happy.",
-    author: "-Jane Fonda"
+    author: "—Jane Fonda"
   },
   {
     quote: "By thought, the thing you want is brought to you; by action, you receive it.",
-    author: "-Wallace D. Wattles"
+    author: "—Wallace D. Wattles"
   },
   {
     quote: "Everything you want is just outside your comfort zone.",
-    author: "-Robert Allen"
+    author: "—Robert Allen"
   },
   {
     quote: "Create a vision of who you want to be, and then live into that picture as if it were already true.",
-    author: "-Arnold Schwarzenegger"
+    author: "—Arnold Schwarzenegger"
   },
   {
     quote: "Believe and act as if it were impossible to fail.",
-    author: "-Charles F. Kettering"
+    author: "—Charles F. Kettering"
   },
   {
     quote: "To fly as fast as thought, to be anywhere there is, you must first begin by knowing that you have already arrived.",
-    author: "-Richard Bach"
+    author: "—Richard Bach"
   },
   {
     quote: "None of us can change our yesterdays, but all of us can change our tomorrows.",
-    author: "-Colin Powell"
+    author: "—Colin Powell"
   },
   {
     quote: "You are today where your thoughts have brought you; you will be tomorrow where your thoughts take you.",
-    author: "-James Allen"
+    author: "—James Allen"
   },
   {
     quote: "Nothing changes until you do.",
-    author: "-Unknown"
+    author: "—Unknown"
   }
 ];
 
